c1115f08e125245c2c04e21d2f610017a82e9171
billettc <charles@cbillette.com>
2024-04-30 14:10:09 -0400

    fix missing google/protobuf/duration.proto

--- src/pb/mod.rs
+++ src/pb/mod.rs
@@ -6,6 +6,13 @@ pub mod cosmos {
         // @@protoc_insertion_point(cosmos.v1)
     }
 }
+pub mod google {
+    // @@protoc_insertion_point(attribute:google.protobuf)
+    pub mod protobuf {
+        include!("google.protobuf.rs");
+        // @@protoc_insertion_point(google.protobuf)
+    }
+}
 pub mod sf {
     pub mod cosmos {
         pub mod r#type {
@@ -14,6 +21,39 @@ pub mod sf {
                 include!("sf.cosmos.type.v2.rs");
                 // @@protoc_insertion_point(sf.cosmos.type.v2)
             }
+        }
+    }
+    // @@protoc_insertion_point(attribute:sf.substreams)
+    pub mod substreams {
+        include!("sf.substreams.rs");
+        // @@protoc_insertion_point(sf.substreams)
+        pub mod index {
+            // @@protoc_insertion_point(attribute:sf.substreams.index.v1)
+            pub mod v1 {
+                include!("sf.substreams.index.v1.rs");
+                // @@protoc_insertion_point(sf.substreams.index.v1)
+            }
+        }
+        pub mod rpc {
+            // @@protoc_insertion_point(attribute:sf.substreams.rpc.v2)
+            pub mod v2 {
+                include!("sf.substreams.rpc.v2.rs");
+                // @@protoc_insertion_point(sf.substreams.rpc.v2)
+            }
+        }
+        pub mod sink {
+            pub mod service {
+                // @@protoc_insertion_point(attribute:sf.substreams.sink.service.v1)
+                pub mod v1 {
+                    include!("sf.substreams.sink.service.v1.rs");
+                    // @@protoc_insertion_point(sf.substreams.sink.service.v1)
+                }
+            }
+        }
+        // @@protoc_insertion_point(attribute:sf.substreams.v1)
+        pub mod v1 {
+            include!("sf.substreams.v1.rs");
+            // @@protoc_insertion_point(sf.substreams.v1)
         }
     }
 }
